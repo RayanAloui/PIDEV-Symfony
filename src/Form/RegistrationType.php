4d10aae970e461f528848522190b0dd124d155dd
louay <elamari.louay@esprit.tn>
2025-03-31 17:12:34 +0100

    Trie Recherche Statistique

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your name.']),
+                ],
+                'label' => 'Name',
+            ])
+            ->add('surname', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your surname.']),
+                ],
+                'label' => 'Surname',
+            ])
+            ->add('telephone', TelType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your telephone number.']),
+                ],
+                'label' => 'Telephone',
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your email address.']),
+                ],
+                'label' => 'Email',
+            ])
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your password.']),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Password must be at least 6 characters long.',
+                    ]),
+                ],
+                'label' => 'Password',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Register',
+                'attr' => ['class' => 'btn btn-primary'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
